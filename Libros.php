495bc476cd8ed5b8aa35625c2cd0d1d9acd4152b
Juan Mora <raicerk@gmail.com>
2015-12-26 14:18:16 -0300

    Se agrega funcion de obtencion de nombre de archivo -ObtienePagina() ademas de validacion de  para evitar error de variables no definidas

--- Libros.php
+++ Libros.php
@@ -2,7 +2,7 @@
 	session_start();
 	include 'Core/Controller.Vista.php';
 	$vista = new Vista();
-	$vista->ValidaAcceso($_SESSION['Usuario'], "Libros.php", "login.php?error=Error de permiso a las busquedas");
+	$vista->ValidaAcceso($_SESSION['Usuario'], $vista->ObtienePagina(), "login.php?error=Error de permiso a las busquedas");
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +22,10 @@
 		<a href="AgregaLibro.php"><input type="button" value="Agregar nuevo libro"></a>
 	</form>
 	<?php
-		echo $vista->BuscaLibros($_GET['txtBuscador'], $_GET['cbEstado']);
+		if (isset($_GET['txtBuscador']) && isset($_GET['cbEstado'])) {
+			echo $vista->BuscaLibros($_GET['txtBuscador'], $_GET['cbEstado']);
+		}
+		
 	?>
 </body>
 </html>
