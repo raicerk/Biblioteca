fd1513adca89cdd98c486ffd63ab67636aed5024
Juan Mora <raicerk@gmail.com>
2016-01-10 17:46:24 -0300

    Se crea archivo inicializador de clases pertenecientes al modelo mas archivo de configuracion del sistema

---
+++ Core/Init.php
@@ -0,0 +1,9 @@
+<?php
+
+#incluye todos los modelos para ser llamados por las vistas
+include 'config.php';
+include 'Modelo.ddbb.php';
+include 'Modelo.sana.php';
+include 'Modelo.utiles.php';
+
+?>
