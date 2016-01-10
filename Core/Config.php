be3c51e00c0c6761d238f53135b9f21902a05109
Juan Mora <raicerk@gmail.com>
2016-01-10 17:46:49 -0300

    Se crea archivo de configuracion de sistema para mejorar la abstraccion del mismo

---
+++ Core/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+#--------------------------------------------------------------------------
+#----------------------- Configuraciones del sistema ----------------------
+#--------------------------------------------------------------------------
+
+
+#Configuracion Base de datos
+define("sqlUsuario", "sa");
+define("sqlContrasena", "R042581796r");
+define("sqlHost", "DESKTOP-L0LTPUU\SQLEXPRESS");
+define("sqlBaseDatos", "");
+
+
+?>
